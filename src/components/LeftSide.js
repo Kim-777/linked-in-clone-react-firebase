5258a3285963a6661a7fed4efb30546198b18098
Kim-777 <iloo3012@naver.com>
2021-05-18 18:33:23 +0900

    UI: header, login

---
+++ src/components/LeftSide.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LeftSide = () => {
+    return (
+        <div>
+            LeftSide
+        </div>
+    )
+};
+
+export default LeftSide;
